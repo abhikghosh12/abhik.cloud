b1a187ffecdf5f846f8e37fc7488808a249ef96a
Abhik Ghosh <abhikghosh87@gmail.com>
2025-10-06 21:44:13 +0200

    added all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+        <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
